92e4ccf065d1a4b3e29cd291ffe2e60fba337c62
Colstin Donaldson <colstin50@gmail.com>
2023-10-06 12:16:00 -0700

    Initial Commit

---
+++ REAL_TALK_CHALLENGE/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  REAL_TALK_CHALLENGE
+//
+//  Created by Colstin Donaldson on 10/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ REAL_TALK_CHALLENGE/REAL_TALK_CHALLENGEApp.swift
@@ -0,0 +1,17 @@
+//
+//  REAL_TALK_CHALLENGEApp.swift
+//  REAL_TALK_CHALLENGE
+//
+//  Created by Colstin Donaldson on 10/6/23.
+//
+
+import SwiftUI
+
+@main
+struct REAL_TALK_CHALLENGEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
